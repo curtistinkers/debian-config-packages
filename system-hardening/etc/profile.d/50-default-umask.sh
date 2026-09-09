34e05d048224088b033ac1916e1590531ea202d5
curtistinkers <curtistinkers.commit@gmail.com>
2026-09-08 20:35:57 -0500

    Add header comments to configuration files for clarity and management

--- system-hardening/etc/profile.d/50-default-umask.sh
+++ system-hardening/etc/profile.d/50-default-umask.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # /etc/profile.d/50-default-umask.sh
+# Managed by debian-config-system-hardening package
 
 # Set default security umask for interactive shells
 umask 027
--- system-hardening/etc/profile.d/50-disable-coredump.sh
+++ system-hardening/etc/profile.d/50-disable-coredump.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # /etc/profile.d/50-disable-coredump.sh
+# Managed by debian-config-system-hardening package
 
 # Disable core dumps for all user shells
 ulimit -S -c 0 >/dev/null 2>&1 || true
